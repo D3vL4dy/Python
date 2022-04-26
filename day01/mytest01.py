ff021cbf60af71e74dd48f38c50168eeb3e9623a
D3vL4dy <kangji1218@kakao.com>
2022-04-26 18:36:29 +0900

    합계

---
+++ day01/mytest01.py
@@ -0,0 +1,12 @@
+# 앞수를 넣으세요 1 Enter
+# 끝수를 넣으세요 10 Enter
+# 당신의 수의 합은 55입니다.
+
+sum = 0
+start = input("앞수를 넣으세요")
+end = input("끝수를 넣으세요")
+
+for i in range(int(start), int(end)+1):
+    sum += i
+
+print("sum", sum)
