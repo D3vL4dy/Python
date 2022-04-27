bfa6beff99645c739e864c8ea7f85f80e92a7279
D3vL4dy <kangji1218@kakao.com>
2022-04-27 18:12:27 +0900

    구구단

--- day01/mytest03.py
+++ day01/mytest03.py
@@ -6,5 +6,6 @@
 num = int(input("출력하고 싶은 단수를 입력하세요"))
 
 for i in range(1, 10):
-    print(num, "*", i, "=", num*i)
+    #print(num, "*", i, "=", num*i)
+    print("{}*{}={}".format(num, i, num*i))
 
