98d5cfd4e09746ad8250b7417597df811db8defe
D3vL4dy <kangji1218@kakao.com>
2022-05-04 14:49:50 +0900

    오목1

--- day07/myomok01.py
+++ day07/myomok01.py
@@ -1,32 +1,40 @@
-#ui를 파이썬에 불러오기
-#오목게임
-
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5 import uic
-from PyQt5.QtGui import QPixmap
+from PyQt5 import uic, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.Qt import QPushButton, QSize
 
-#UI파일 연결
-form_class = uic.loadUiType("myomok01.ui")[0]
+form_class = uic.loadUiType("myomok0.ui")[0]
 
-#화면을 띄우는데 사용되는 Class 선언
-class MainClass(QMainWindow, form_class) :
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("GUI")
-        
-        self.pixmap = QPixmap('0.png')
+class MainClass(QMainWindow, form_class):
+    def __init__(self) :
+        QMainWindow.__init__(self)
+        self.flag_wb = True
+        self.setupUi(self)
         
         for i in range(10):
-            self.label = QLabel(self)
-            self.label.setPixmap(self.pixmap)
-            self.label.resize(self.pixmap.width(), self.pixmap.height())
-            self.show()
-            
+            for j in range(10):
+                obj = QPushButton(self)
+                obj.setText('')
+                obj.setIcon(QtGui.QIcon('0.png'))
+                obj.setIconSize(QSize(40, 40))
+                obj.setGeometry(0 + 40*j, 0 + 40*i, 40, 40)
+                obj.clicked.connect(self.myclick)
         
-
+        self.pb.clicked.connect(self.myreset)
+        self.show()
+        
+    def myclick(self):
+        if self.flag_wb:
+            self.sender().setIcon(QtGui.QIcon('1.png'))
+        else:
+            self.sender().setIcon(QtGui.QIcon('2.png'))
+        self.flag_wb = not self.flag_wb
+        
+    def myreset(self):
+        print("myreset")
+            
+                      
 if __name__ == "__main__" :
-    app = QApplication(sys.argv)
-    window = MainClass()
+    app = QApplication(sys.argv) 
+    window = MainClass() 
     app.exec_()
-    
